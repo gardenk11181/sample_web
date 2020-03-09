2a3cabfb469ec4461e25a254738a464c51a0c4e9
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-03-09 11:13:05 +0900

    Add WebView

--- app/src/main/java/com/gardenlab/sampleweb/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleweb/MainActivity.java
@@ -3,12 +3,53 @@ package com.gardenlab.sampleweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    WebView webView;
+    final String url ="https://www.naver.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        webView = findViewById(R.id.webView);
+        editText.setText(url);
+
+        // 웹뷰 설정 수정
+        final WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new ViewClient());
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                webView.loadUrl(editText.getText().toString());
+
+            }
+        });
+    }
+
+    private class ViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+
+            return true;
+        }
+
     }
 }
